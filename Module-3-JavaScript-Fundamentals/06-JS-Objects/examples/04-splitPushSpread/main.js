9e7e449b7ab963aca490317412330fb8c8dc27ee
Joshua Gregory <yoshig5114@gmail.com>
2023-05-25 17:05:35 -0400

    js splitPush example

--- Module-3-JavaScript-Fundamentals/06-JS-Objects/examples/04-splitPushSpread/main.js
+++ Module-3-JavaScript-Fundamentals/06-JS-Objects/examples/04-splitPushSpread/main.js
@@ -1,9 +1,9 @@
-let lyrics = "As I walk down the valley of the Shadow of Death I take a look at my life and realize there's nothing left"
+// let lyrics = "As I walk down the valley of the Shadow of Death I take a look at my life and realize there's nothing left"
 
-let arrSplit = lyrics.split(' ')
-arrSplit[10] = "Death ,"
-arrSplit = arrSplit.join(' ').split(',')
-console.log(arrSplit);
+// let arrSplit = lyrics.split(' ')
+// arrSplit[10] = "Death ,"
+// arrSplit = arrSplit.join(' ').split(',')
+// console.log(arrSplit);
 
 // for(let i = arrSplit.length -1; i>0 ; i-- ){
 //     console.log(arrSplit[i]);
@@ -12,7 +12,7 @@ console.log(arrSplit);
 let lyrics2 = "cause i been blasting and laughin so low that even my mama thinks my mind is gone"
 
 let arr2Split = lyrics2.split(' ');
-//arrSplit.push(arr2Split)
-// let newArray = [...arrSplit, ...arr2Split]
+arrSplit.push(arr2Split)
+let newArray = [...arrSplit, ...arr2Split]
 
-//console.log(arrSplit)
+console.log(arrSplit)
