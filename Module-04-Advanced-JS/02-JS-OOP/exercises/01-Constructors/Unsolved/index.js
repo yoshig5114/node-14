d18cdeee862f74dee3b3b50a827c1d4dc630d48a
Joshua Gregory <yoshig5114@gmail.com>
2023-05-31 14:22:01 -0400

    oop exercise

--- Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
+++ Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
@@ -1,7 +1,14 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
-
+function Developer(name, tech){
+    this.name = name;
+    this.tech = tech;
+    this.introduction = () => {
+        console.log(`Hello, my name is ${name}, and my favorite tech is ${tech}.`)
+    }
+}
 // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
 
 // TODO: Create a new object using the 'Developer' constructor
-
+let noobCoder = new Developer("Josh","Python");
 // TODO: Call the 'introduction()' method on the new object
+noobCoder.introduction();
